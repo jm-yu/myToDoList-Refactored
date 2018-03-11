dde45aeda7766cc8ed553f03ce663f85e529ef63
jmyu1994 <jmyu1994@gmail.com>
2018-03-11 16:45:13 -0400

    add notification channel

--- app/src/main/java/jmyu/ufl/edu/mytodolist/AlarmReceiver.java
+++ app/src/main/java/jmyu/ufl/edu/mytodolist/AlarmReceiver.java
@@ -1,11 +1,13 @@
 package jmyu.ufl.edu.mytodolist;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
@@ -20,10 +22,26 @@ import jmyu.ufl.edu.mytodolist.models.Todo;
 public class AlarmReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
+        final String CHANNEL_ID = "jmyu.ufl.edu.mytodolist.alert";
         final int notificationId = 100;
         Todo todo = intent.getParcelableExtra(TodoEditActivity.KEY_TODO);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            CharSequence name = context.getString(R.string.channel_name);
+            String description = context.getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system
+            nm.createNotificationChannel(channel);
+            Log.i("jmyu demo", Integer.toString(Build.VERSION.SDK_INT));
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_notifications_black_24dp)
                 .setContentTitle(todo.text)
                 .setContentText(todo.text);
@@ -40,8 +58,6 @@ public class AlarmReceiver extends BroadcastReceiver{
 
         builder.setContentIntent(resultPendingIntent);
 
-        NotificationManager nm = (NotificationManager) context.getSystemService(
-                Context.NOTIFICATION_SERVICE);
         // notificationId allows you to update the notification later on, like canceling it
         nm.notify(notificationId, builder.build());
 
